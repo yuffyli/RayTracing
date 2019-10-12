fb41eb0b47d8e580b9c0527e5736464839aea307
yuffyli <yuffy_lee@163.com>
2019-10-12 13:22:40 +0800

    Add Project RayTracing in one weekend

---
+++ project/RayTracingInOneWeekend/RayTracingInOneWeekend/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  RayTracingInOneWeekend
+//
+//  Created by Yuffy on 2019/10/11.
+//  Copyright © 2019 none. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int nx = 200;
+    int ny = 100;
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    for (int j = ny-1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float r = float(i) / float(nx);
+            float g = float(j) / float(ny);
+            float b = 0.2;
+            int ir = int(255.99*r);
+            int ig = int(255.99*g);
+            int ib = int(255.99*b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
+    return 0;
+}
